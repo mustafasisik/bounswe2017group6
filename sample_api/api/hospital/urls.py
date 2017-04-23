1aefa113248ccddbf5373bc2f6f314c327fb77a5
leblebi1 <berkay.kozan@boun.edu.tr>
2017-04-23 21:36:33 +0300

    Berkay's update
    patient single get and put requests are added

--- sample_api/api/hospital/urls.py
+++ sample_api/api/hospital/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^doctor/$', views.doctor, name='doctor'),
     url(r'^doctor/([0-9]+)/$', views.doctor_single, name='doctor'),
+    url(r'^patient/([0-9]+)/$', views.patient_single, name='patient'),
+
 ]
--- sample_api/api/hospital/views.py
+++ sample_api/api/hospital/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
+import json
 
 # Create your views here.
 @csrf_exempt
@@ -20,3 +21,33 @@ def doctor_single(request, doctor_id):
 
     elif request.method == "POST":
         pass
+
+@csrf_exempt
+def patient_single(request, patient_id):
+    if request.method == "GET":
+        print(request)
+        patient = Patient.objects.filter(id=int(patient_id))
+        if patient.exists():
+            patient = patient.first()
+        else:
+            pass
+        return JsonResponse({"name":patient.name, "lastname":patient.lastname, "age":patient.age})
+        
+    elif request.method == "PUT":
+        data = json.loads(request.body.decode("utf-8"))
+        patient = Patient.objects.filter(id=int(patient_id))
+        if patient.exists():
+            patient = patient.first()
+        else:
+            return JsonResponse({"status":"FAIL", "message":"patient does not exist"})
+        try:
+            if "age" in data:
+                patient.age = data["age"]
+            if "name" in data:
+                patient.name = data["name"]
+            if "lastname" in data:
+                patient.lastname = data["lastname"]
+            patient.save()
+        except:
+            return JsonResponse({"status":"FAIL", "message":"missing field"})
+        return JsonResponse({"status":"OK", "message":""})
