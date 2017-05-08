5905abaeb951ec993522fe1806281ac4781b476d
Yusuf Hakan Kalayci <yhkalayci@gmail.com>
2017-05-09 02:03:20 +0300

    doctor get/put testcases are added.

--- sample_api/api/hospital/tests.py
+++ sample_api/api/hospital/tests.py
@@ -2,6 +2,8 @@ import unittest
 import os
 import subprocess
 from django.test import TestCase
+from .models import *
+import json
 
 class Test1(TestCase):
 	def test_rendezvous_get(self):
@@ -13,3 +15,47 @@ class Test1(TestCase):
 		expected='{"status": "OK", "message": "rendezvous added"}'
 		self.assertEqual(expected,result)
 
+
+class DoctorSingleTestCase(TestCase):
+    def setUp(self):
+        Doctor.objects.create(name="Deniz", lastname="Yilmaz", age="38")
+        Doctor.objects.create(name="Firat", lastname="Tekin", age="29")
+        Doctor.objects.create(name="Selin", lastname="Kir", age="41")
+
+    def test_doctor_single_get(self):
+        resp = self.client.get("/hospital/doctor/1/")
+        expected = b'{"name": "Deniz", "lastname": "Yilmaz", "age": 38}'
+        self.assertEqual(expected, resp.content)
+
+        resp = self.client.get("/hospital/doctor/2/")
+        expected = b'{"name": "Firat", "lastname": "Tekin", "age": 29}'
+        self.assertEqual(expected, resp.content)
+
+        resp = self.client.get("/hospital/doctor/3/")
+        expected = b'{"name": "Selin", "lastname": "Kir", "age": 41}'
+        self.assertEqual(expected, resp.content)
+
+    def test_doctor_single_put(self):
+        data = {"name": "Asya", "lastname": "Tarik", "age": "33"}
+        resp = self.client.put("/hospital/doctor/1/",
+                               json.dumps(data),
+                               content_type="application/json")
+        expected = b'{"status": "OK", "message": ""}'
+        self.assertEqual(expected, resp.content)
+
+        data = {"name": "Asya", "lastname": "Tarik"}
+        resp = self.client.put("/hospital/doctor/2/",
+                               json.dumps(data),
+                               content_type="application/json")
+        expected = b'{"status": "OK", "message": ""}'
+        self.assertEqual(expected, resp.content)
+
+        data = {"name": "Asya", "lastname": "Tarik", "age": "33"}
+        resp = self.client.put("/hospital/doctor/6/",
+                               json.dumps(data),
+                               content_type="application/json")
+        expected = b'{"status": "FAIL", "message": "doctor does not exist"}'
+        self.assertEqual(expected, resp.content)
+
+    def tearDown(self):
+        Doctor.objects.all().delete()
