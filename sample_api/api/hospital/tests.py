d390666e3c7b901ce5032ebb97842b34f1659892
mfbalin <m.f.balin@gmail.com>
2017-05-09 02:02:47 +0300

    Added department get and put unit tests

--- sample_api/api/hospital/tests.py
+++ sample_api/api/hospital/tests.py
@@ -2,6 +2,11 @@ import unittest
 import os
 import subprocess
 from django.test import TestCase
+from django.test.client import RequestFactory
+from .views import department_single, department
+from .models import Department
+from django.http import JsonResponse
+import json
 
 class Test1(TestCase):
 	def test_rendezvous_get(self):
@@ -13,3 +18,22 @@ class Test1(TestCase):
 		expected='{"status": "OK", "message": "rendezvous added"}'
 		self.assertEqual(expected,result)
 
+
+class DepartmentTestCase(TestCase):
+	def setUp(self):
+		self.factory = RequestFactory()
+		Department.objects.create(name = "yuz kafa bas")
+		Department.objects.create(name = "deliler kismi")
+	
+	def test_get_method(self):
+		request = self.factory.get('/hospital/department/')
+		response = department_single(request, 1)
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.content, JsonResponse({"name": "yuz kafa bas"}).content)
+	
+	def test_put_method(self):
+		request = self.factory.put('/hospital/department/', json.dumps({"name": "tirnak burun sac"}), content_type = 'application/json')
+		response = department_single(request, 1)
+		self.assertEqual(response.status_code, 200)
+		department = Department.objects.filter(id = int(1)).first()
+		self.assertEqual("tirnak burun sac", department.name)
